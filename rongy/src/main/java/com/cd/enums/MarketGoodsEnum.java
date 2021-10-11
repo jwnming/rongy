5d9a77391320991b0887e93582bfabc28a610415
jwnming <jwnmingjava@163.com>
2021-10-11 21:16:19 +0800

    #1011【代码优化】跳蚤市场枚举注释完善 20211011 211644

--- rongy/src/main/java/com/cd/enums/MarketGoodsEnum.java
+++ rongy/src/main/java/com/cd/enums/MarketGoodsEnum.java
@@ -2,17 +2,19 @@ package com.cd.enums;
 
 public enum MarketGoodsEnum {
 
-    //跳蚤市场发布商品信息操作
+    // 跳蚤市场发布商品信息操作
     TZSC_GOODS_DEAL_TYPE$FINISHED("001","已完成"),
     TZSC_GOODS_DEAL_TYPE$DELETE("002", "删除"),
 
-    //跳蚤市场发布商品状态
+    // 跳蚤市场发布商品状态
     TZSC_GOODS_STATUS$DOING("1", "活动中"),
     TZSC_GOODS_STATUS$DONE("2", "已完成"),
 
+    // 状态
     DELETE_FLAG$NO("0", "正常"),
     DELETE_FLAG$YES("1", "删除"),
 
+    // 行为
     TZSC_GOODS_PUBLISH_TYPE$BUY("1", "求购"),
     TZSC_GOODS_PUBLISH_TYPE$SELL("2", "出售"),
     ;
